88a1a3a9272cef22fc26d59e03f88a5c41f22b59
Jhammett418 <JHammett866@boyslatin.net>
2024-10-29 19:17:49 +0000

    notes js practice

---
+++ unit 2/jsPractice.js
@@ -0,0 +1,22 @@
+// Datatypes are the basic building
+// block of Java script programs
+
+//strings
+// strings are a piece of dat that is wrapped in
+// quotation marks.
+
+"double quotes tell the computer that this is a string"
+"my name is Jamal"
+"309090"
+"1!#$!#$"
+'single quotes can also be used to write strings.'
+console.log('my name is Jamal');
+console.log("This is some text...")
+// Integers
+//i integers are pieces of data that represent numbers.
+23
+345
+1023
+3209472095702970
+console.log('3*12')
+//Booleans
