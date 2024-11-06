fa47d6d7a2b5b9608695a92f0cfa44f5a9e91096
Jhammett418 <JHammett866@boyslatin.net>
2024-11-06 20:12:36 +0000

    asyncActivity work

---
+++ unit 2/asyncActivity_nov1.js
@@ -0,0 +1,27 @@
+// 1.
+ "var -" 
+"let -"
+"const - this a variable where the data needs to remain constant. cannot change" 
+
+// 2. 
+// keyword- word we kmow
+// variable
+// data types
+//console.log
+
+// context - what is it asking us to do
+// multiply 3 vars that will result in 4000
+var a = 2
+var b = 2
+var c = 1000
+
+console.log("this message will appear in terminal")
+console.log(a* b* c)
+
+// 3.
+
+"let - email draft that your still working on- can still change  "
+" const - email that has been sent- cannot change"
+
+//4.
+"remebering coding, learn how to do it myself, figure out errors"
