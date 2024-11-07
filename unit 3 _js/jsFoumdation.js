9ef478ed49eaf92dbaabf85446fbf566fb44485d
Jhammett418 <JHammett866@boyslatin.net>
2024-11-07 20:13:17 +0000

    javaacrptvaribaleconvention

---
+++ unit 3 _js/jsFoumdation.js
@@ -0,0 +1,18 @@
+var example = 'this is a string' // anything wrapped in quotes
+var example2 = true // boolean = true and false 
+var example3 = 128932 // integers =numbers.
+  
+// var is short for variable-
+// it is the universal keyword for as variable
+// when you create a variable you always need to start with 'var'
+// Or use 'let' or 'const' But these have special contextual usage.
+var userAccount = 'Jamal'
+var userAddress = '123 main street'
+
+var userphoneNumberPrimary = 12321321
+var userphoneNumbersecondary =56747647887
+var userphoneNumber= ''
+let='' // special usade- data is expected to change.
+ const='' // special usage- data is not expected to change
+ // camel case = every word after the first is capitalized
+ tiktok_account_name 'Jamal123'
