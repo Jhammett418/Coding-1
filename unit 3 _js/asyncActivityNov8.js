f47677dd622486c2741de132e093546ab65ffd91
Jhammett418 <JHammett866@boyslatin.net>
2024-11-12 20:17:10 +0000

    asyncreview

--- unit 3 _js/asyncActivityNov8.js
+++ unit 3 _js/asyncActivityNov8.js
@@ -1,3 +1,28 @@
 // what is string concatenation mean?
 ' strings concentenation is to add together plus the addition off adding a variable'
-  
+   
+
+// # 3. define each of following variable name formats then provide 
+
+//# -camel case - every word after the first word is capitalized.
+var first_Name_Last_Name=''
+// # - snake case - every word is seperarted with an underscore.
+var firstNameLastName=''
+
+// # 4.
+let a = 350
+
+ let b =a * 2
+ console.log(b) // the output in the terminal should be 700.
+
+ // # 5 
+
+var x= 10 // 10
+
+var y= 2 // 4
+
+var z= y*2 // 4
+ console.log(x > 1 && z= 4)
+
+ // is 10 greater than 1= true
+ // 
