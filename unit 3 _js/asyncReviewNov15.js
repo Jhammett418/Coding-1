d22a731c0cefb3c026b6a3b04cef41285e64986f
Jhammett418 <JHammett866@boyslatin.net>
2024-11-18 18:56:25 +0000

    asyncreveiw

---
+++ unit 3 _js/asyncReviewNov15.js
@@ -0,0 +1,26 @@
+var username =''
+var Password =''
+
+var  securitySystemUsername ='Jamal'
+var  securitySystemPasscode='123'
+ console.log(username)
+
+
+
+// 2
+'comparison, because we need to comapare if we have more or less than'
+'what we ordered.'
+ var ecgOrderRequest= 3
+ var ecgShipmentRecieved= 20
+
+ // 3
+ 'assignment operator'
+ var messageTosend = 'hello class'
+ var messageToBeRec= messageToSend
+
+
+ // 4
+
+ 'singler equal sign: assignment operator'
+  'double ampersand: logical And operator'
+           
