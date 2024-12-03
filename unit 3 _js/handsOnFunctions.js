e16f9ca22fd4c2f9824cebc670483f49cde86a75
Jhammett418 <JHammett866@boyslatin.net>
2024-12-03 20:17:42 +0000

    functionpractice

---
+++ unit 3 _js/handsOnFunctions.js
@@ -0,0 +1,37 @@
+// Anatomy of a function
+
+//curly brackets= this is
+
+// called a parameter. we pass data
+
+// inside of funcftions parameters
+
+// parameter= placeholder for data
+
+//data = just
+
+
+function PizzaParty(name, topping) {
+    console.log('congrats' + name + "you have been invited to the pizza party.")
+    console.log("your custom pizza will have" + topping + "on it.")
+}
+
+PizzaParty('Jamal', 'chesse')
+
+// Conditional statements in Js
+
+//conditional statements are written using
+// the 'if' and 'else' keywords. this alows 
+// use to make secisions based on data
+// our function recieves.
+
+function PizzaPartyinvitation(name) {
+    let guest = "mike";
+    if (name != guest) {
+        console.log("this name is not on the list");
+    } else {
+        console.log("welcome to the party");
+    }
+}
+
+PizzaPartyinvitation()
