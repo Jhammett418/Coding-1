02a4c283b484d04c48cb027888c221a553f7587a
Jhammett418 <JHammett866@boyslatin.net>
2024-12-19 20:32:17 +0000

    holidayplanner

---
+++ unit 3 _js/HolidayExtraCredit.js
@@ -0,0 +1,22 @@
+function holidayPlanner(date) {
+    console.log('Holiday event schedule')
+
+    if (date == 21) {
+        console.log('on the 21st I will chill.');
+    } else if (date == 30) {
+        console.log("2nd day of break");
+    } else if (date == 3) {
+        console.log('on the 3rd im going to visit family')
+    } else {
+        console.log('sorry, this is not a date on the planner.');
+    }
+
+}
+
+holidayPlanner(3);
+
+
+// function positiveOrNegative(number) {
+//     throw new Error("Function not implemented.");
+// }
+
