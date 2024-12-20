0b01f2766d1bc9151842c4f171cffc97bf2fc271
Jhammett418 <JHammett866@boyslatin.net>
2024-12-20 20:24:23 +0000

    quiz

---
+++ Quarter22_quiz.js
@@ -0,0 +1,14 @@
+ // In your own words, describe what a function is. Please write your response in a complete sentence as a string data type.
+// this is where the functions message can be quotationed
+
+//In your own words, describe the difference between a function parameter and a function argument. Please write your response in a complete sentence as a string data typee.
+//a function parameter is some thing to pass data and a function arguement is another method of the same thing.
+
+// in your own words, describe what an if/ else conditional statement is. Please write your response in a complete sentence as a string data type.
+// this alows use to make secisions based on data.
+
+// What is an integer data type? Please write your response in a complete sentence as a string data type.
+("pieces of dat that represents numbers")
+
+//What is a boolean data type? Please write your response in a complete sentence as a string data type.
+("this help the function understand values")
