210bbc8b912dbc549d7986e1a2e12edc42a7f8a4
Jhammett418 <JHammett866@boyslatin.net>
2025-01-08 20:18:20 +0000

    javascriotloop.js

---
+++ unit4/javascriptLoops.js
@@ -0,0 +1,14 @@
+function sophmore(year){ 
+  if(year==1){
+    console.log(" your a freshman");
+  } else if ("year==2."){
+    console.log("sophmore.");
+  }else if (year==3){
+    console.log('your a junior');
+  }
+
+    //console.log ('testing function to see if it works:' )
+}
+
+
+collegeTitle(7)
