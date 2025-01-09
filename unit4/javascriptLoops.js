40e254da94650ee91aba69fa2949544b94a0f2c4
Jhammett418 <JHammett866@boyslatin.net>
2025-01-09 20:13:15 +0000

    jsloops practice

--- unit4/javascriptLoops.js
+++ unit4/javascriptLoops.js
@@ -5,6 +5,10 @@ function sophmore(year){
     console.log("sophmore.");
   }else if (year==3){
     console.log('your a junior');
+  }else if(year == 4){
+    console.log('your a senior');
+  }else if(year=5){
+    console.log ("your in a masters program")
   }
 
     //console.log ('testing function to see if it works:' )
@@ -12,3 +16,26 @@ function sophmore(year){
 
 
 collegeTitle(7)
+
+// a JS list is a data type that is used to group and
+// organize other data types into one variable
+
+// we use the square brackets to designate something as 
+// a list.
+var grocerylist= [ 'water', 'apple', "ground beef"]
+var RandomData =[Jamal,'coding class',
+true,
+false,
+ ]
+ console.log(RandomData)
+ coding1s=['black cw,'green cw','white cw']
+ coding2s=['red cw ','bluecw']
+ codings3s=[]
+
+ console.log('theses are the coding sneakers we have in stock')
+ console.log(codings1 , codings2 , codings3,)
+
+ cleaning supplies= ['dish soap', 'paper towels',]
+ deli meats = ['turkey', 'ham',]
+
+ 
