d05f372e07236dca3d9233e5f97af192ebcd041a
Jhammett418 <JHammett866@boyslatin.net>
2025-01-15 20:15:48 +0000

    Arraymethod

---
+++ unit4/jsArrayMethods.js
@@ -0,0 +1,36 @@
+// They are more flexible compared to other programming
+// language list data types.
+
+// In python/ Java they have List, Dict, Set, Tuple
+// which are under the array family.But these list 
+// data types all have a special job or function, where as 
+// in JS it universal.
+
+// Js Array is a data type for collecting and storing
+// other data
+
+var name = 'Jamal'
+var grocery = ['apple', 'milk', 'water']
+// An array can store
+var generalList = [name, grocery, 132323, 23.34, true];
+
+// Array Methods
+// A method a function that works on a list (or object).
+// methods are written after the list variable name and 
+// declared with period
+//pretty much; the method is going inside of the list
+// and running instructions on data from the list
+console.log()
+console.error()
+
+// push method()
+generalList.push('New data')
+console.log(generaList)
+
+// Pop method() -This method removes a piece of data
+// from alist. It will remove the last item in that list.
+generalList.pop()
+console.log(generalList)
+  //length() method - this method counts the number of data 
+  // inside of a list
+  console.log(generalList. length)
