a5a7b65081128f9c11d0cd82d7a518982122a230
Jhammett418 <JHammett866@boyslatin.net>
2025-01-30 20:16:38 +0000

    calculatorApp

---
+++ unit4/calculator.js
@@ -0,0 +1,9 @@
+function calculate( numberA, arithmethicOperator,number8){
+if( arithemthicOperator =="+"){
+    console.log('we are doing addition')
+    var result = numberA + numberB
+    console.log(result);
+}else if (arithemeticOperator == "-"){
+    
+}
+}
